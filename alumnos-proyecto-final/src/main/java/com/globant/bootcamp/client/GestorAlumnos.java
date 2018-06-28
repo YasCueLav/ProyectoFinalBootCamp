bf3d963f435c45a8ffb793f0d16810093b72990e
YasCueLav <yaslavcue@gmail.com>
2018-06-28 19:41:49 -0300

    conexion Listo

---
+++ alumnos-proyecto-final/src/main/java/com/globant/bootcamp/client/GestorAlumnos.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.globant.bootcamp.client;
+
+import com.globant.bootcamp.domain.Alumno;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Yasmin
+ */
+public class GestorAlumnos {
+    private Connection conn;
+
+    public GestorAlumnos() {
+        AccesoDatos ad = new AccesoDatos();
+        try {
+            conn = DriverManager.getConnection(ad.getConn_string(), ad.getUser(), ad.getPass());
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public ArrayList<Alumno> obtenerAlumnos (){
+        ArrayList<Alumno> lista = new ArrayList<>();
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet query = stmt.executeQuery("Select * from Alumnos");
+            while (query.next()){
+                Alumno a = new Alumno();
+                a.setIdAlumno(query.getInt("id_alumno"));
+                a.setLegajo(query.getInt("legajo"));
+                a.setNombre(query.getString("nombre"));
+                a.setApellido(query.getString("apellido"));
+                a.setDocumento(query.getInt("documento"));
+                a.setFechaNace(query.getString("fecha_nacimiento"));
+                lista.add(a);
+            }
+            query.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return lista;
+    }
+    
+    public Alumno obtenerAlumnos (int id){
+        Alumno a = new Alumno();
+        try{
+            PreparedStatement stmt = conn.prepareStatement("select * from Alumnos where id_alumno = ?");
+            stmt.setInt(1, id);
+            ResultSet query = stmt.executeQuery();
+            if(query.next()){
+                a.setIdAlumno(query.getInt("id_alumno"));
+                a.setLegajo(query.getInt("legajo"));
+                a.setNombre(query.getString("nombre"));
+                a.setApellido(query.getString("apellido"));
+                a.setDocumento(query.getInt("documento"));
+                a.setFechaNace(query.getString("fecha_nacimiento"));
+            }
+            query.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return a;
+    }
+    
+    public boolean modificarAlumno (Alumno a) {
+        boolean modifico = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE Alumnos SET legajo = ?, nombre = ?, apellido = ?, documento = ?, fecha_nacimiento = ? WHERE id_alumno = ?");
+            stmt.setInt(1, a.getLegajo());
+            stmt.setString(2, a.getNombre());
+            stmt.setString(3, a.getApellido());
+            stmt.setInt(4, a.getDocumento());
+            stmt.setString(5, a.getFechaNace());
+            stmt.setInt(6, a.getIdAlumno());
+            stmt.executeUpdate();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            modifico = false;
+        }
+        return modifico;
+    }
+    
+    public boolean eliminarAlumno (Alumno a) {
+        boolean eliminar = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM Alumnos WHERE id_alumno = ?");
+            stmt.setInt(1, a.getIdAlumno());
+            stmt.executeUpdate();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            eliminar = false;
+        }
+        return eliminar;
+    }
+}
