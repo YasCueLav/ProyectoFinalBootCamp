cd93b02cada9be7106f3a1c2b9877839b4221ae2
YasCueLav <yaslavcue@gmail.com>
2018-06-28 23:37:26 -0300

    completo metodos

--- alumnos-proyecto-final/src/main/java/com/globant/bootcamp/service/service.java
+++ alumnos-proyecto-final/src/main/java/com/globant/bootcamp/service/service.java
@@ -7,11 +7,11 @@ package com.globant.bootcamp.service;
 
 import com.globant.bootcamp.client.GestorAlumnos;
 import com.globant.bootcamp.domain.Alumno;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javax.ws.rs.*;
+import javax.ws.rs.QueryParam;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
  * @author Yasmin
  */
 @Component
-@Path("/alumno")
+@Path("/service")
 @Consumes("application/json")
 @Produces("application/json")
 public class service {
@@ -43,14 +43,21 @@ public class service {
     }
     
     @GET
-    @Path("/alumno/getOneAlumno/{legajo}")
+    @Path("/alumno/getAll")
+    private Map getAllAlumno() {
+         return this.init();
+    }
+    
+    
+    @GET
+    @Path("/alumno/getOne/{legajo}")
     private Alumno getOneAlumno(@PathParam("legajo") int legajo) {
         a = ga.obtenerAlumnos(legajo);
         return a;
     }
     
     @GET
-    @Path("/alumno/deleteAlumno/{legajo}")
+    @Path("/alumno/delete/{legajo}")
     private String deleteAlumno (@PathParam("legajo") int legajo) {
         boolean elimino;
         String txt;
@@ -66,4 +73,32 @@ public class service {
         }
     }
     
+    @GET
+    @Path("/alumno/update/{leg}")
+    private String updateAlumno (@PathParam("leg") int leg, @QueryParam("legajo") int legajo,
+			@QueryParam("nombre") String nombre, @QueryParam("apellido") String apellido,
+                        @QueryParam("documento") int documento, @QueryParam("fecha") String fecha) {
+        boolean modifico;
+        String txt = "";
+        
+        Alumno alu = new Alumno();
+        a = ga.obtenerAlumnos(leg);
+        
+        alu.setIdAlumno(a.getIdAlumno());
+        alu.setLegajo(legajo);
+        alu.setNombre(nombre);
+        alu.setApellido(apellido);
+        alu.setDocumento(documento);
+        alu.setFechaNace(fecha);
+        
+        modifico = ga.modificarAlumno(alu);
+        
+        if (modifico) {
+            txt = "Modifiado";
+            return txt;
+        }else{
+            txt = "No se ha Modifiado";
+            return txt;
+        }
+    }
 }
